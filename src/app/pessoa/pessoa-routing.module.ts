69d291ad1cdf2adfc7ab5276c9980a7277ce7304
Bruno Urbano Rodrigues <brunourb@gmail.com>
2023-11-14 19:03:06 -0300

    feat: correção componente empresa dentro de pessoa

--- src/app/pessoa/pessoa-routing.module.ts
+++ src/app/pessoa/pessoa-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmpresaListComponent } from '../empresa/empresa-list/empresa-list.component';
-import { EmpresaDetailComponent } from '../empresa/empresa-detail/empresa-detail.component';
+import { PessoaListComponent } from './pessoa-list/pessoa-list.component';
+import { PessoaDetailComponent } from './pessoa-detail/pessoa-detail.component';
 
 const routes: Routes = [
   {
-    path: '', component: EmpresaListComponent
+    path: '', component: PessoaListComponent
   },
   {
-    path: 'detail', component: EmpresaDetailComponent
+    path: 'detail', component: PessoaDetailComponent
   }
 ];
 
